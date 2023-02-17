d86da6cb6293fcfa347a971aa5cee73cddf9ef0e
rasulovsardor123 <rasulovs861@gmail.com>
2023-02-17 18:07:37 +0500

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-bu son kvadratni aniqlaydigan funksiya
+# bu son kvadratni aniqlaydigan funksiya
 def kvadrat(x, n):
     print(x**n)
 kvadrat(12, 6)
 
 
-bu son kupaytmani aniqlash dasturi
+# bu son kupaytmani aniqlash dasturi
 def kupaytma(*numbers):
     result =1
     for number in numbers:
         result= result * number
     print(result)
 kupaytma(2,2,2)
+
+
+
+# pycharmdan yozdim
 
